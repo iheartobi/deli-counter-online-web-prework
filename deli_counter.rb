b6d2e43a2a1ba4bebb90cb98b261b43a290fcfbb
Ititi Obidah <iobidah9@gmail.com>
2019-04-11 01:36:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,34 @@
-# Write your code here.
 katz_deli = []
-def line(katz_deli) 
-  if katz_deli.empty?
-  puts "The line is currently empty."
-else
- current_line = []
- number = 1 
- katz_deli.each do |customer|
-   current_line << "#{number}. #{customer}"
-   number += 1 
- end
- puts "The line is currently: #{current_line.join(" ")}"
- end
-end
 
-def take_a_number(katz_deli, name)
-    katz_deli << name
-    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+def line(katz_deli)
+  if katz_deli.size == 0 
+   puts "The line is currently empty."
+ else 
+   current_line = "The line is currently:"
+   number = 0
+   katz_deli.each do |name|
+     current_line << " #{number += 1}. #{name}"
+   end
+     puts current_line
+ end
 end 
-def now_serving(katz_deli)
-  if katz_deli.empty?
+
+def take_a_number(katz_deli, name) 
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end 
+
+def now_serving(katz_deli) 
+  if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else 
-  puts "Currently serving #{katz_deli.first}."
-  katz_deli.shift
- end
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+ end 
 end
+counter = 0 
+def take_a_number_2(katz_deli)
+  counter += 1 
+  katz_deli << counter 
+  puts "Welcome. Your are number #{counter}"
+end 
